6c85e6447c0734783422a75de95a35ea178ed56f
Sergio Pichardo <sp88work@gmail.com>
2020-04-28 13:55:53 -0500

    change port to 8080

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
     res.send({ message: "Hi there" })
